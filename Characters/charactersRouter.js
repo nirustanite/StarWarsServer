f532f5775a53927c8693b86d4c9e1bc2d54c89ea
nirupamaa jaya <nirustanite@gmail.com>
2020-05-23 18:24:20 +0200

    code for sorting

--- Characters/charactersRouter.js
+++ Characters/charactersRouter.js
@@ -5,6 +5,10 @@ const router = new Router();
 const Promise = require('bluebird')
 const _ = require('lodash')
 
+function compare(a,b){
+  return b.height - a.height
+}
+
 router.get('/films/:id/characters', async (req, res, next) => {
 
    const sortoption = req.query.sort
@@ -14,12 +18,8 @@ router.get('/films/:id/characters', async (req, res, next) => {
     const filmDetail = await rp({url, json:true})
     const charactersPromise = filmDetail.characters.map(url => rp({url, json:true}))
     const characters = await Promise.map(charactersPromise, character => _.pick(character, ['name', 'height', 'gender']))
-
-    sortoption === 'desc' ? res.status(200).send(characters.sort(function(a, b){
-        return b.height-a.height
-    })) : res.status(200).send(characters.sort(function(a,b){ 
-        return a.height-b.height
-    }))
+    const sortedcharacter = characters.sort(compare);
+    sortoption === 'desc' ? res.status(200).send(sortedcharacter) : res.status(200).send(sortedcharacter.reverse())
    }catch(e) {
     res.status(500).send(e.message)
    }
